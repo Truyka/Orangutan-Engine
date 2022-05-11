79dc09309af3510a2c1560d01f63f515a3e6d0cd
Truyka <mmilewski2202@gmail.com>
2022-05-11 20:57:52 +0200

    Added more exact relative path

--- include/Components.h
+++ include/Components.h
@@ -1,14 +1,14 @@
 #ifndef __COMPONENTS_H__
 #define __COMPONENTS_H__
 
-#include "Animation.h"
-#include "Camera.h"
-#include "Collider.h"
-#include "Follow.h"
-#include "Meta.h"
-#include "Physics.h"
-#include "Script.h"
-#include "Sprite.h"
-#include "Transform.h"
+#include "components/Animation.h"
+#include "components/Camera.h"
+#include "components/Collider.h"
+#include "components/Follow.h"
+#include "components/Meta.h"
+#include "components/Physics.h"
+#include "components/Script.h"
+#include "components/Sprite.h"
+#include "components/Transform.h"
 
 #endif // __COMPONENTS_H__
