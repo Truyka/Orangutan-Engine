6e9e3bda1f94b06621fedcca15a25687d5bc4b5f
Truyka <mmilewski2202@gmail.com>
2022-05-11 20:56:01 +0200

    A single include for all the components

---
+++ include/Components.h
@@ -0,0 +1,14 @@
+#ifndef __COMPONENTS_H__
+#define __COMPONENTS_H__
+
+#include "Animation.h"
+#include "Camera.h"
+#include "Collider.h"
+#include "Follow.h"
+#include "Meta.h"
+#include "Physics.h"
+#include "Script.h"
+#include "Sprite.h"
+#include "Transform.h"
+
+#endif // __COMPONENTS_H__
